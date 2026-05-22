64d30901f466fe215651312b8dccc9b051649f22
Peter Knapp <peterknapp.developer@gmail.com>
2026-05-22 07:47:53 +0200

    Initial package

---
+++ node.lua
@@ -0,0 +1,12 @@
+gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+local font = resource.load_font "font.ttf"
+local text = "Hallo Welt"
+
+util.json_watch("config.json", function(config)
+    text = config.text
+end)
+
+function node.render()
+    gl.clear(0,0,0,1)
+    font:write(100, 100, text, 64, 1,1,1,1)
+end
